02f29f6e0f54bdaa52ca3a5a54fbb32f6881641e
jessyca-machado <jessycamachado0@gmail.com>
2026-03-17 00:10:12 -0300

    feat: add utility functions and constants

---
+++ src/utils/constants.py
@@ -0,0 +1,41 @@
+URL = (
+        "https://raw.githubusercontent.com/"
+        "IBM/telco-customer-churn-on-icp4d/master/data/Telco-Customer-Churn.csv"
+    )
+
+FEATURES_COLS = [
+        "SeniorCitizen",
+        "gender",
+        "Partner",
+        "Dependents",
+        "tenure",
+        "PhoneService",
+        "MultipleLines",
+        "InternetService",
+        "OnlineSecurity",
+        "OnlineBackup",
+        "DeviceProtection",
+        "TechSupport",
+        "StreamingTV",
+        "StreamingMovies",
+        "Contract",
+        "PaperlessBilling",
+        "PaymentMethod",
+        "MonthlyCharges",
+        "TotalCharges",
+    ]
+
+YES_NO_COLS = [
+        "Partner",
+        "Dependents",
+        "PhoneService",
+        "MultipleLines",
+        "OnlineSecurity",
+        "OnlineBackup",
+        "DeviceProtection",
+        "TechSupport",
+        "StreamingTV",
+        "StreamingMovies",
+        "PaperlessBilling",
+        "Churn",
+    ]
